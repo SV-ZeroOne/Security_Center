6ac4a510ccea3042e52c2fe99e9775b3b92507d2
Steve Velcev <steve@elitehost.co.za>
2020-07-09 21:13:46 +0200

    change database to postgresql and dbname  of security_center_db and install psycopg2 db connector

--- security_center/settings.py
+++ security_center/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'security_center.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'security_center_db',
+        'USER': 'postgres',
+        'PASSWORD': 'TheCodingOne',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
